d10f8cc22fc6cc6712c187140cd79126bec57f37
natthan <natthandosantos@gmail.com>
2023-08-16 16:44:06 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/ProjetoOM_LPOO_2023_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.bcc.lpoo.om;
+
+/**
+ *
+ * @author 20221PF.CC0041
+ */
+public class ProjetoOM_LPOO_2023_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Servico.java
@@ -0,0 +1,14 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+/**
+ *
+ * @author 20221PF.CC0041
+ */
+public class Servico {
+
+    public Servico() {
+        
+    }
+    
+    
+}
