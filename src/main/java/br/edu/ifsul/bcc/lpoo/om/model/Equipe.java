1e4a43d4e483ebbaffacc97147791a3a796c54e5
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:02:51 -0300

    add Equipe java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Equipe.java
@@ -0,0 +1,31 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+public class Equipe {
+    private Integer id;
+    private String nome;
+    private String especialidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+}
