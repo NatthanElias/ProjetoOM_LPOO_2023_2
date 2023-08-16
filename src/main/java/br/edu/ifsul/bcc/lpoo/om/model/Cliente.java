543cb83e4acc5c0d50756d94b668fd860928e267
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:18:56 -0300

    add Cliente java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Cliente.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+public class Cliente extends Pessoa{
+    private String observacoes;
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+}
