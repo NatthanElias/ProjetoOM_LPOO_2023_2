3c7d7cff6049e70d3ae8351d07aa889d0889344e
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:19:36 -0300

    add MaoObra java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/MaoObra.java
@@ -0,0 +1,42 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+import java.util.Date;
+
+public class MaoObra {
+    private Integer id;
+    private String descricao;
+    private Date tempo_estimado_execucao;
+    private Float valor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getTempo_estimado_execucao() {
+        return tempo_estimado_execucao;
+    }
+
+    public void setTempo_estimado_execucao(Date tempo_estimado_execucao) {
+        this.tempo_estimado_execucao = tempo_estimado_execucao;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+}
