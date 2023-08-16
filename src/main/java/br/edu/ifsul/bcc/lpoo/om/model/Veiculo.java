9d39e1cdb60111bf7e0068034f07c0550bc38c4f
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:19:52 -0300

    add Veiculo java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Veiculo.java
@@ -0,0 +1,42 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+import java.util.Calendar;
+
+public class Veiculo {
+    private String placa;
+    private String modelo;
+    private Integer ano;
+    private Calendar data_ultimo_servico;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public Calendar getData_ultimo_servico() {
+        return data_ultimo_servico;
+    }
+
+    public void setData_ultimo_servico(Calendar data_ultimo_servico) {
+        this.data_ultimo_servico = data_ultimo_servico;
+    }
+}
