b7047c922ca29a9ec11233f6b148f07fe1892dc5
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:05:07 -0300

    add Pessoa java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Pessoa.java
@@ -0,0 +1,61 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+import java.util.Calendar;
+
+public abstract class Pessoa {
+
+    private String cpf;
+    private String nome;
+    private String senha;
+    private Calendar data_nascimento;
+    private String cep;
+    private String complemento;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Calendar getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Calendar data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
