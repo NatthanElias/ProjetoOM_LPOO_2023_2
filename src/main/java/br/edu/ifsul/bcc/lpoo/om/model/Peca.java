f065e87d5431a5e8ab3fd8096afb450c32bfe363
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:19:43 -0300

    add Peca java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Peca.java
@@ -0,0 +1,40 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+public class Peca {
+    private Integer id;
+    private String nome;
+    private Float valor;
+    private String fornecedor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+}
