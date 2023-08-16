69ed3087f286fd58c4921d56f44f399f635938fa
NatthanElias <natthandosantos@gmail.com>
2023-08-16 18:19:28 -0300

    add Funcionario java class

---
+++ src/main/java/br/edu/ifsul/bcc/lpoo/om/model/Funcionario.java
@@ -0,0 +1,33 @@
+package br.edu.ifsul.bcc.lpoo.om.model;
+
+import java.util.Calendar;
+
+public class Funcionario extends Pessoa{
+    private String numero_ctps;
+    private Calendar data_admissao;
+    private Calendar data_demissao;
+
+    public String getNumero_ctps() {
+        return numero_ctps;
+    }
+
+    public void setNumero_ctps(String numero_ctps) {
+        this.numero_ctps = numero_ctps;
+    }
+
+    public Calendar getData_admissao() {
+        return data_admissao;
+    }
+
+    public void setData_admissao(Calendar data_admissao) {
+        this.data_admissao = data_admissao;
+    }
+
+    public Calendar getData_demissao() {
+        return data_demissao;
+    }
+
+    public void setData_demissao(Calendar data_demissao) {
+        this.data_demissao = data_demissao;
+    }
+}
